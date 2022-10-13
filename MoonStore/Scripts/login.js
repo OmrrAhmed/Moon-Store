02449adff2cfde084f70854a4ad627ed0f342a16
Omar Ahmed <eng.omarahmed@outlook.com>
2022-10-13 15:34:38 +0200

    Add project files.

---
+++ MoonStore/Scripts/login.js
@@ -0,0 +1,18 @@
+﻿function login()
+{
+    var user = document.getElementById('Username').value;
+    var password = document.getElementById('Password').value;
+
+    if (user == "OmarAhmed" && password == "Oo@42403828") {
+
+        window.location.href = "../Pages/Home.html";
+        alert("Login Successfully");
+        return false;
+
+    }
+
+    else {
+        alert("Please Try Again..")
+    }
+
+}
